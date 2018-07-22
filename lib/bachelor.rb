19d5c36f4b999e175177b7d72b549150c7792f8c
Felix Chan <felixchan29@gmail.com>
2018-07-22 21:45:56 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,7 +53,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_num, people|
     if season_num == season
       people.each do |person|
-        avg_age += person["age"]
+        avg_age += person["age"].to_i
       end
     end
   end
